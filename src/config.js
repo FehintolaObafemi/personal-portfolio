735d427597a4a1101fda340e2734f3da8cf9cd28
Taofeek F. Obafemi-Babatunde <taofeekobafemibabatunde@gmail.com>
2024-05-24 02:03:16 -0500

    updating

--- src/config.js
+++ src/config.js
@@ -29,7 +29,7 @@ module.exports = {
         },
         {
             name: 'Website',
-            url: 'https://fobafemi.netlify.app/',
+            url: 'https://fehintola.netlify.app/',
         },
     ],
 
